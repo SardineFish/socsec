2693bcfc3776edcaa247f5649d3df26a6356180c
SardineFish <Sardine@live.cn>
2017-12-18 23:10:41 +0800

    Add an jumping & moving animation

---
+++ socsec/Assets/Editor/EnemySpawnerEditor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEditor;
+
+namespace Assets.Editor
+{
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(EnemySpawner))]
+    public class EnemySpawnerEditor:UnityEditor.Editor
+    {
+        
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+            var spawner = target as EnemySpawner;
+            var spawnPoints = new List<GameObject>();
+            for(var i = 0; i < spawner.transform.childCount; i++)
+            {
+                if (spawner.transform.GetChild(i).tag == "Waypoints")
+                    spawnPoints.Add(spawner.transform.GetChild(i).gameObject);
+            }
+            spawner.SpawnPoints = spawnPoints.ToArray();
+            //spawner.SpawnPoint = EditorGUILayout.ObjectField("Spawn Point:", spawner.SpawnPoint, typeof(GameObject), true) as GameObject;
+            
+
+        }
+    }
+}
---
+++ socsec/Assets/Script/EnemySpawner.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class EnemySpawner: MonoBehaviour
+{
+    public GameObject[] SpawnPoints;
+    public float SpawnDuration = 1;
+    public bool RandomSpawn = false;
+    public float RandomRange = 0;
+    public GameObject[] SpawnableEnemies;
+    public int[] SpawnWeight;
+
+    float nextSpawnTime = 0;
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
