c83d1fc426119b643fb0450f2f55b65c2b31afb2
SardineFish <Sardine@live.cn>
2017-12-19 21:38:23 +0800

    Complete custom editor for EnemySpawner

--- socsec/Assets/Editor/EnemySpawnerEditor.cs
+++ socsec/Assets/Editor/EnemySpawnerEditor.cs
@@ -11,21 +11,80 @@ namespace Assets.Editor
     [CustomEditor(typeof(EnemySpawner))]
     public class EnemySpawnerEditor:UnityEditor.Editor
     {
-        
+        bool showSpawnPoints = false;
+        bool showSpawnableEnemies = true;
         public override void OnInspectorGUI()
         {
-            DrawDefaultInspector();
+            //DrawDefaultInspector();
             var spawner = target as EnemySpawner;
             var spawnPoints = new List<GameObject>();
             for(var i = 0; i < spawner.transform.childCount; i++)
             {
-                if (spawner.transform.GetChild(i).tag == "Waypoints")
+                if (spawner.transform.GetChild(i).tag == "Waypoint")
                     spawnPoints.Add(spawner.transform.GetChild(i).gameObject);
             }
             spawner.SpawnPoints = spawnPoints.ToArray();
             //spawner.SpawnPoint = EditorGUILayout.ObjectField("Spawn Point:", spawner.SpawnPoint, typeof(GameObject), true) as GameObject;
             
 
+            showSpawnPoints = EditorGUILayout.Foldout(showSpawnPoints, "Spawn Points: " + spawner.SpawnPoints.Length.ToString());
+            if (showSpawnPoints)
+            {
+                GUIStyle style = new GUIStyle();
+                style.margin.left = 20;
+                EditorGUILayout.BeginVertical(style);
+                foreach (var point in spawner.SpawnPoints)
+                {
+                    EditorGUILayout.ObjectField(point, typeof(GameObject), true);
+                }
+                EditorGUILayout.EndVertical();
+            }
+
+            EditorGUILayout.Space();
+
+            spawner.SpawnRadius = EditorGUILayout.FloatField("Spawn Radius", spawner.SpawnRadius);
+            spawner.SpawnDuration = EditorGUILayout.FloatField("Spawn Duration", spawner.SpawnDuration);
+            spawner.RandomSpawn = EditorGUILayout.Toggle("Random Time", spawner.RandomSpawn);
+            spawner.RandomRange = EditorGUILayout.FloatField("Range of Time", spawner.RandomRange);
+
+            EditorGUILayout.Space();
+
+            showSpawnableEnemies = EditorGUILayout.Foldout(showSpawnableEnemies, "Spawnable Enemies: " + spawner.SpawnableEnemies.Length.ToString());
+            if (showSpawnableEnemies)
+            {
+
+                GUIStyle style = new GUIStyle();
+                style.margin.left = 20;
+                EditorGUILayout.BeginVertical(style);
+
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.LabelField("Count:", GUILayout.Width(60));
+                var count = EditorGUILayout.IntField(spawner.SpawnableEnemies.Length, GUILayout.Width(60));
+                if (GUILayout.Button("+", GUILayout.Width(30)))
+                    count++;
+                if (GUILayout.Button("-", GUILayout.Width(30)))
+                    count--;
+                EditorGUILayout.EndHorizontal();
+                if (count != spawner.SpawnableEnemies.Length)
+                {
+                    Array.Resize(ref spawner.SpawnableEnemies, count);
+                    Array.Resize(ref spawner.SpawnWeight, count);
+                }
+
+                for(var i = 0; i < count; i++)
+                {
+                    EditorGUILayout.BeginHorizontal();
+                    spawner.SpawnableEnemies[i] = EditorGUILayout.ObjectField(spawner.SpawnableEnemies[i], typeof(GameObject), true,GUILayout.Width(150)) as GameObject;
+                    //EditorGUILayout.Space();
+                    EditorGUILayout.LabelField("Spawn Weight:", GUILayout.Width(90));
+                    spawner.SpawnWeight[i] = EditorGUILayout.IntField(spawner.SpawnWeight[i]);
+                    EditorGUILayout.EndHorizontal();
+                }
+
+                EditorGUILayout.EndVertical();
+            }
+
+            EditorGUILayout.Space();
         }
     }
 }
--- socsec/Assets/Script/EnemySpawner.cs
+++ socsec/Assets/Script/EnemySpawner.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 public class EnemySpawner: MonoBehaviour
 {
     public GameObject[] SpawnPoints;
+    public float SpawnRadius = 1;
     public float SpawnDuration = 1;
     public bool RandomSpawn = false;
     public float RandomRange = 0;
@@ -21,6 +22,23 @@ public class EnemySpawner: MonoBehaviour
 
     private void Update()
     {
-        
+        if (Time.time > nextSpawnTime)
+        {
+            if (RandomSpawn)
+                nextSpawnTime += SpawnDuration + UnityEngine.Random.Range(-RandomRange, RandomRange);
+            else
+                nextSpawnTime += SpawnDuration;
+            
+
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        foreach(var point in SpawnPoints)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(point.transform.position, SpawnRadius);
+        }
     }
 }
